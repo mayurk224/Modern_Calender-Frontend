fa0853ed1b1c5531a061f63533b219dab8f43ae9
Mayur Kamble <mayurkamble0250@gmail.com>
2024-11-02 21:03:30 +0530

    days added

---
+++ src/components/Day.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Day = () => {
+  return (
+    <div>
+      <div className="flex flex-nowrap gap-2">
+        <h3 className="text-lg font-semibold px-2 py-1 rounded-3xl bg-orange-400">
+          Sunday
+        </h3>
+        <h3 className="text-lg font-semibold px-2 py-1 rounded-3xl bg-orange-400">
+          Monday
+        </h3>
+        <h3 className="text-lg font-semibold px-2 py-1 rounded-3xl bg-orange-400">
+          Tuesday
+        </h3>
+        <h3 className="text-lg font-semibold px-2 py-1 rounded-3xl bg-orange-400">
+          Wednesday
+        </h3>
+        <h3 className="text-lg font-semibold px-2 py-1 rounded-3xl bg-orange-400">
+          Thursday
+        </h3>
+        <h3 className="text-lg font-semibold px-2 py-1 rounded-3xl bg-orange-400">
+          Friday
+        </h3>
+        <h3 className="text-lg font-semibold px-2 py-1 rounded-3xl bg-orange-400">
+          Saturday
+        </h3>
+      </div>
+    </div>
+  );
+};
+
+export default Day;
--- src/sections/Calender.jsx
+++ src/sections/Calender.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import CalenderHeader from "../components/CalenderHeader";
+import Day_Track from "../components/Day";
 
 const Calender = () => {
   return (
     <div className="flex items-center justify-center h-screen">
-      <div className="bg-gray-400 w-[40%] h-[90%] rounded-2xl p-4">
-        <CalenderHeader />
+      <div className="bg-gray-400 w-[50%] h-[90%] rounded-2xl p-4">
+        <div className="">
+          <CalenderHeader />
+        </div>
+        <div className="mt-4">
+          <Day_Track />
+        </div>
       </div>
     </div>
   );
